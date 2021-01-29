a097da3fba1eea83cfc67a69d3f81c8b5e60d23e
Alejandra Villanueva <alejyvilla@gmail.com>
2021-01-28 21:52:39 -0500

    ls pwd commands

---
+++ bash.js
@@ -0,0 +1,21 @@
+const pwd = require('./pwd');
+const ls = require('./ls');
+
+process.stdout.write('prompt > ');
+
+process.stdin.on('data', (data) => {
+  const cmd = data.toString().trim();
+
+  switch (cmd) {
+    case 'pwd':
+      pwd();
+      break;
+    case 'ls':
+      ls();
+      break;
+    default:
+      process.stdout.write('You typed: ' + cmd);
+      process.stdout.write('\nprompt > ');
+  }
+  //
+});
